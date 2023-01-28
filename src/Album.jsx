0ba48a53f74ce188acf70c2610a5a652886a8450
badr ghanbi <bghanbi50@gmail.com>
2023-01-28 16:06:11 +0100

    testing diffrent component and how to implement them for the whole spotify clone

--- src/Album.jsx
+++ src/Album.jsx
@@ -4,6 +4,7 @@ const Album = (props) => {
   return (
     <div>
     <p>{props.trackName}</p>
+    <img src={props.image} />
     </div>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,28 @@ import axios from "axios";
 import Album from './Album';
 function App() {
 
-   const [albums,setAlbums]=useState([])
-   const options = {
-    method: 'GET',
-    url: 'https://spotify81.p.rapidapi.com/search',
-    params: {
-      q: '<REQUIRED>',
-      type: 'multi',
-      offset: '0',
-      limit: '30',
-      numberOfTopResults: '5'
-    },
-    headers: {
-      'X-RapidAPI-Key': '981133e597mshf67d3a4955b7df3p109b23jsn70341f1012a1',
-      'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-    }
-  };
+   const [albums,setAlbums]=useState([]);
+  const [seachQuery,setSeachQuery]=useState("soul");      
+const options = {
+  method: 'GET',
+  url: 'https://spotify23.p.rapidapi.com/search/',
+  params: {
+    q: seachQuery,
+    type: 'multi',
+    offset: '0',
+    limit: '16',
+    numberOfTopResults: '5'
+  },
+  headers: {
+    'X-RapidAPI-Key': '981133e597mshf67d3a4955b7df3p109b23jsn70341f1012a1',
+    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+  }
+};
+
+const handleSelectChange=(event)=>{
+  setSeachQuery(event.target.value);
+  console.log(event.target.value);
+}
 
 useEffect(()=>{
   
@@ -29,14 +35,20 @@ useEffect(()=>{
     }).catch(function (error) {
       console.error(error);
     });
-},[])
+},[seachQuery])
 
 return (
   <>
-  <h1>yoo</h1>
-    <section className='wrapper'>
+  <select onChange={handleSelectChange}>
+  <option value="soul" >Soul</option>
+  <option value="rock">Rock</option>
+  <option value="pop">Pop</option>
+  <option value="classic">Classic</option>
+</select>
+
+    <section className='wrapper grid grid-cols-3 gap-4'>
    {    albums.map(({data})=>{
-   return <Album  trackName={data.name} />
+   return <Album  trackName={data.name} image={data.coverArt.sources[0].url} />
    }
    )} 
    </section> 
