c1d040e7f49cf7260365cb8abd3c8780b30aee7d
badr ghanbi <bghanbi50@gmail.com>
2023-01-30 12:46:53 +0100

    add elementary styles for the header section

--- src/componenet/Header/Header.jsx
+++ src/componenet/Header/Header.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DictionaireLogo from '../Main/DictionaireLogo'
+import Moon from './Moon'
 const Header = () => {
+const [darkMode,setDarkMode] =useState(true)
+  const toggleDarkMode=() => {
+    setDarkMode(prv => !prv)
+  }
+
   return (
     <header >
-      <h1 >hello world</h1>
-      <nav>
+      <nav className='flex items-center justify-between'>
        <DictionaireLogo /> 
-       <div className='custom-select'>
-        <select className={` bg-black p-2 text-white border-0`} >
+       <div className='flex items-center justify-between'>
+        <select className={` bg-black w-20 text-white border-0`} >
           <option className='hover:text-accent' value="sans serif">Sans Serif</option>
           <option className='hover:text-accent' value="serif">Serif</option>
           <option className='hover:text-accent' value="mono" selected>Mono</option>
 
         </select>
+        <div onClick={toggleDarkMode} className='flex items-center'>
+        <div className='h-5 switchModeWrraper mx-4 w-10 rounded-full  bg-accent cursor-pointer flex items-center px-1 '>
+          <span className={`h-4 toggler  bg-white w-4 block rounded-full ${darkMode ? 'translate-x-full' :''}`}></span>
+        </div>
+        <Moon/>
+       </div>
        </div>
       </nav>
     </header>
